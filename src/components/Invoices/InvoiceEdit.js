ae6f1007679e82e7b57297ea55689d1d1f8efef6
javadoffuad <javadoffuad@gmail.com>
2017-10-22 15:26:38 +0300

    invoice edit

--- src/components/Invoices/InvoiceEdit.js
+++ src/components/Invoices/InvoiceEdit.js
@@ -1,144 +1,18 @@
 import React from 'react';
-import { connect } from 'react-redux'
-import Select from 'react-select';
 
-import { Grid, Row, Col, FormGroup, FormControl, ControlLabel, Button, Table } from 'react-bootstrap';
+import { Grid, Row, Col } from 'react-bootstrap';
 
-class InvoiceEdit extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            customer: '',
-            product: '',
-            productsToOrder: []
-        }
-    }
-
-    componentDidMount() {
-		document.title = "InvoiceApp | Edit invoice";
-	}
-
-    updateState(element) {
-        this.setState({
-            customer: element
-        });
-    }
-
-    updateStateProduct(element) {
-        this.setState({
-            product: element
-        });
-    }
-    
-    render() {
-        console.log("customers", this.props.customers)
-        console.log("products", this.props.products)
-
-        //console.log(this.props.match.params.id)
-
-        var options = [];
-        var optionsProduct = [];
-
-        this.props.customers.forEach(function(item, i) {
-            options.push({
-                    value: item.id,
-                    label: item.name
-                })
-        });
-
-        this.props.products.forEach(function(item, i) {
-            optionsProduct.push({
-                value: item.id,
-                label: item.name,
-                price: item.price
-            })
-        });
-
-        return(
-            <Grid>
+export default ({match}) => {
+    return <Grid>
                 <Row>
                     <Col md={12}>
-                        <h1>Edit invoice</h1>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={2}>
-                        <FormGroup controlId="discount">
-                            <ControlLabel>Discount (%)</ControlLabel>
-                            <FormControl
-                                type="text"
-                                placeholder="Enter text"/>
-                        </FormGroup>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={4}>
-                        <FormGroup controlId="customer">
-                            <ControlLabel>Customer</ControlLabel>
-                            <Select
-                                name="form-field-name"
-                                value={this.state.customer}
-                                options={options}
-                                onChange={this.updateState.bind(this)}
-                                />
-                        </FormGroup>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={4}>
-                        <FormGroup controlId="product">
-                            <ControlLabel>Add product</ControlLabel>
-                            <Select
-                            name="form-field-name2"
-                            value={this.state.product}
-                            options={optionsProduct}
-                            onChange={this.updateStateProduct.bind(this)}
-                            />
-                        </FormGroup>
-                    </Col>
-                    <Col>
-                        <Button>Add</Button>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={6}>
-                        <Table responsive>
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Price</th>
-                                    <th>Qty</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>John Doe</td>
-                                    <td>100</td>
-                                    <td width="180">
-                                    <FormControl
-                                        defaultValue="1"
-                                        type="text"
-                                        placeholder="Enter text"/>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </Table>
+                        <h1>New invoice</h1>
                     </Col>
                 </Row>
                 <Row>
                     <Col md={12}>
-                        <h1>Total: 1000</h1>
+                        <p>invoice id {match.params.id}</p>
                     </Col>
                 </Row>
             </Grid>
-        )
-    }
 }
-
-export default connect(
-    state => ({
-		invoices: state.invoiceReducer,
-		customers: state.customerReducer,
-		products: state.productReducer,
-	})
-)(InvoiceEdit);
